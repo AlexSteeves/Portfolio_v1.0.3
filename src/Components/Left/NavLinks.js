8e57b7c3d7d83337930b39ef7775e00a5a6b86ec
A-Steeves <48775637+AlexSteeves@users.noreply.github.com>
2023-11-09 11:53:44 -0500

    res

--- src/Components/Left/NavLinks.js
+++ src/Components/Left/NavLinks.js
@@ -12,7 +12,7 @@ export default function NavLinks() {
           <a href="https://www.linkedin.com/in/alexander-steeves-8122b8178/"><BsLinkedin /></a>
         </li>
       </ul>
-      <h1 className="flex justify-center items-center pt-2 text-xl underline underline-offset-2 font-thin"><link href={process.env.PUBLIC_URL + 'pictures/Steeves_Resume_V1.0.3.pdf'}>Resume</link></h1>
+      <h1 className="flex justify-center items-center pt-2 text-xl underline underline-offset-2 font-thin"><a target = '_blank' href={process.env.PUBLIC_URL + 'pictures/Steeves_Resume_V1.0.3.pdf'}>Resume</a></h1>
     </div >
   );
 }
