5a4289cddb52b4b34d3b58905422ce433e6abb42
A-Steeves <48775637+AlexSteeves@users.noreply.github.com>
2023-11-16 23:51:53 -0500

    changes

--- src/Components/Left/HeaderText.js
+++ src/Components/Left/HeaderText.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function HeaderText() {
   return (
-    <div className="p-8 lg:pt-20 font-sans text-center">
+    <div className="p-8 pt-32 font-sans text-center">
       <img src={process.env.PUBLIC_URL + 'pictures/Headshot.png'} className="w-52 h-52 mx-auto rounded-full" alt="Headshot" />
 
       <h1 className="font-light text-3xl lg:text-5xl text-red-100 drop-shadow-md tracking-tight mt-4">
