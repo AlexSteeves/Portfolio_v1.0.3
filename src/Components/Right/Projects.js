dbea72df3dea77396b58050a1bd5889713a58be3
A-Steeves <48775637+AlexSteeves@users.noreply.github.com>
2023-11-18 21:21:21 -0500

    experience

--- src/Components/Right/Projects.js
+++ src/Components/Right/Projects.js
@@ -3,10 +3,10 @@ import { projects } from "../../Data/ExperienceData";
 function Projects() {
   return (
     <section id="projects" className="p-5 h-full px-10 text-red-100">
-      <h1 className="mb-4 font-thin text-xl border-b-2 border-slate-300 pb-2 ">Projects</h1>
+      <h1 className="mb-4 font-thin text-xl border-b-2 border-slate-300 pb-2">Projects</h1>
       {projects.map((proj, index) => (
         <a href={proj.link} key={index}>
-          <div className="p-4 m-4 rounded-lg flex items-center">
+          <div className="project-item p-8 m-4 rounded-lg flex items-center hover:bg-red-400 duration-300">
             <div className="w-[20%] mr-4">
               <img
                 src={process.env.PUBLIC_URL + proj.picture}
@@ -14,10 +14,10 @@ function Projects() {
                 alt={proj.title}
               />
             </div>
-            <div className = 'text-left w-[80%]'>
+            <div className='text-left w-[80%]'>
               <h2 className="text-xl font-normal">{proj.title}</h2>
               <p className="font-thin pt-1">{proj.description}</p>
-              <p className = "font-thin pt-1">Skills: <span className=" font-normal">{proj.tools}</span></p>
+              <p className="font-thin pt-1">Skills: <span className="font-normal">{proj.tools}</span></p>
             </div>
           </div>
         </a>
