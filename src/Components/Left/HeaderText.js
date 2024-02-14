a71d740097472ccfda53e811a19a0656006fb476
A-Steeves <48775637+AlexSteeves@users.noreply.github.com>
2024-02-13 23:02:11 -0500

    Image Formatting

--- src/Components/Left/HeaderText.js
+++ src/Components/Left/HeaderText.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-
-
 export default function HeaderText() {
     return (
-        <div className="p-8  text-center ">
-            <img
-                src={process.env.PUBLIC_URL + "pictures/Headshot.png"}
-                className=" w-24 h-24 md:w-36 md:h-36 lg:w-48 lg:h-48 mx-auto rounded-full"
-                alt="Headshot"
-            />
+        <div className="px-14 p-8 lg:p-8  text-left lg:text-center ">
+            <div className="flex justify-start items-start md:justify-center md:items-center">
+                <img
+                    src={process.env.PUBLIC_URL + "pictures/Headshot.png"}
+                    className="w-24 h-24 md:w-36 md:h-36 lg:w-48 lg:h-48 mx-auto rounded-full"
+                    alt="Headshot"
+                />
+            </div>
 
             <h1 className="header-text">Alexander Steeves</h1>
 
             <h2 className="subheader-text">
-                Fullstack Developer and Computer Engineering Graduate
+                Avid Developer and Computer Engineering Graduate
             </h2>
         </div>
     );
